5e95f41a92a9b8774eda949493ac560a348d0733
chinaheyu <810130242@qq.com>
2024-03-01 10:51:35 +0800

    support python 3.6

--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
-from setuptools import Extension, setup
+from setuptools import Extension, setup, find_packages
+import sys
 
-setup(
-    ext_modules=[
-        Extension(
-            name="cv2_enumerate_cameras._cv2_enumerate_cameras",
-            sources=["src/cv2_enumerate_cameras/cv2_enumerate_cameras.cpp"]
-        )
-    ]
-)
+
+if sys.hexversion < 0x3070000:
+    setup(
+        name='cv2_enumerate_cameras',
+        description='Enumerate / List / Find / Detect / Search index for opencv VideoCapture.',
+        version='1.1.4',
+        package_dir={"": "src"},
+        packages=find_packages('src'),
+        ext_modules=[
+            Extension(
+                name="cv2_enumerate_cameras._cv2_enumerate_cameras",
+                sources=["src/cv2_enumerate_cameras/cv2_enumerate_cameras.cpp"]
+            )
+        ]
+    )
+else:
+    setup(
+        ext_modules=[
+            Extension(
+                name="cv2_enumerate_cameras._cv2_enumerate_cameras",
+                sources=["src/cv2_enumerate_cameras/cv2_enumerate_cameras.cpp"]
+            )
+        ]
+    )
